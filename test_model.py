20dcb6b53a3c66a5ace51f272c7dc10fd69f0ec8
dolphywind <yunusaydin590@gmail.com>
2025-11-25 14:26:45 +0300

    Make some changes to experiment code

--- test_model.py
+++ test_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import AutoModel, AutoTokenizer, RobertaTokenizerFast
+from sentence_transformers import SentenceTransformer, util
 
 
 def show_params(model: nn.Module):
@@ -38,8 +39,8 @@ def embedding_test(tokenizer: RobertaTokenizerFast, model: nn.Module, device: to
     tokenizer_out: dict[str, torch.Tensor] = tokenizer(sentences, padding=True, return_tensors='pt')
     token_ids: torch.Tensor = tokenizer_out['input_ids'].to(device)
     attn_mask: torch.Tensor = tokenizer_out['attention_mask'].to(device)
-    # model_in: torch.Tensor = torch.tensor(token_ids, dtype=torch.long).to(device)
-    out = model(token_ids, attention_mask=attn_mask).pooler_output
+    model_in: torch.Tensor = torch.tensor(token_ids, dtype=torch.long).to(device)
+    out = model(token_ids, attn_mask).pooler_output
     diff_1 = (out[0] - out[1]).unsqueeze(0)
     diff_2 = (out[2] - out[3]).unsqueeze(0)
     sim: float = F.cosine_similarity(diff_1, diff_2).item()
@@ -50,28 +51,31 @@ def embedding_test(tokenizer: RobertaTokenizerFast, model: nn.Module, device: to
 def real_embedding_test(tokenizer: RobertaTokenizerFast, model: nn.Module, device: torch.device):
     sentences = [
         'istanbul, boğaziçi köprüsüyle iki kıtayı birleştiren, tarihi ve kültürel zenginlikleriyle ünlü bir şehirdir.',
-        'istanbul, boğaziçi köprüsüyle iki kıtayı birleştiren, tarihi ve kültürel zenginlikleriyle ünlü bir şehir değildir.'
+        'istanbul boğaziçi'
     ]
-    tokenizer_out: dict[str, torch.Tensor] = tokenizer(sentences, padding=True, return_tensors='pt')
-    token_ids: torch.Tensor = tokenizer_out['input_ids'].to(device)
-    attn_mask: torch.Tensor = tokenizer_out['attention_mask'].to(device)
+    # tokenizer_out: dict[str, torch.Tensor] = tokenizer(sentences, padding=True, return_tensors='pt')
+    # token_ids: torch.Tensor = tokenizer_out['input_ids'].to(device)
+    # attn_mask: torch.Tensor = tokenizer_out['attention_mask'].to(device)
     # model_in: torch.Tensor = torch.tensor(token_ids, dtype=torch.long).to(device)
-    out = model(token_ids, attention_mask=attn_mask).last_hidden_state
-    attn_mask_unsq: torch.Tensor = attn_mask.unsqueeze(-1)
-    out = (out * attn_mask_unsq).sum(dim=1) / attn_mask_unsq.sum(dim=1)
-    v1 = out[0].unsqueeze(0)
-    v2 = out[1].unsqueeze(0)
-    sim: float = F.cosine_similarity(v1, v2).item()
+    out = model.encode(sentences)
+    # attn_mask_unsq: torch.Tensor = attn_mask.unsqueeze(-1)
+    # out = (out * attn_mask_unsq).sum(dim=1) / attn_mask_unsq.sum(dim=1)
+    # v1 = out[0].unsqueeze(0)
+    # v2 = out[1].unsqueeze(0)
+    sim: float = util.cos_sim(out[0], out[1]).item()
     print(f"Similarity score: {sim}")
 
 
 def main():
     device: torch.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Device: {device}")
-    MODEL_NAME = 'TURKCELL/roberta-base-turkish-uncased'
+    MODEL_NAME: str = 'TURKCELL/roberta-base-turkish-uncased'
     MODEL_NAME: str = 'dbmdz/bert-base-turkish-128k-uncased'
+    MODEL_NAME: str = 'emrecan/bert-base-turkish-cased-mean-nli-stsb-tr'
+    # https://huggingface.co/emrecan/bert-base-turkish-cased-mean-nli-stsb-tr
     tokenizer: RobertaTokenizerFast = AutoTokenizer.from_pretrained(MODEL_NAME)
-    model: nn.Module = AutoModel.from_pretrained(MODEL_NAME).to(device)
+    # model: nn.Module = AutoModel.from_pretrained(MODEL_NAME).to(device)
+    model: nn.Module = SentenceTransformer(MODEL_NAME)
 
     # show_params(model)
     # inf_message_loop(tokenizer, model, device)
